d08e556d5a200fa2a241d216588dea2d3f91be32
96simben <simoncraft3@hotmail.com>
2020-03-01 11:28:03 +0100

    complete

--- src/piljetter/BuyTickets.java
+++ src/piljetter/BuyTickets.java
@@ -1,4 +1,4 @@
-package piljetter;
+package com.company;
 
 import javax.swing.*;
 import java.awt.*;
@@ -10,7 +10,7 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
 
-public class BuyTickets {
+public class buyTickets {
 
         static JPanel buyTicketsPanel = new JPanel();
 
@@ -18,12 +18,15 @@ public class BuyTickets {
         String buttonTextVenue;
         String buttonTextDate;
         String buttonTextCountry;
+        String buttonConcertId;
 
         String searchword;
-        JTextField search = new JTextField("search here");
+        JTextField search = new JTextField("");
+        static public JTextField ticketToBuy = new JTextField("Ticket to buy");
         JButton searchbutton = new JButton("search");
+        JButton couponButton = new JButton("use coupon");
 
-        int i = 1;
+        static boolean coupon = false;
 
 
         public void allConcerts() {
@@ -38,6 +41,10 @@ public class BuyTickets {
                 search.setVisible(true);
                 buyTicketsPanel.add(search);
 
+                ticketToBuy.setBounds(750, 10, 100, 25);
+                ticketToBuy.setVisible(true);
+                buyTicketsPanel.add(ticketToBuy);
+
                 //coonfirm search button
                 searchbutton.setForeground(Color.black);
                 searchbutton.setBackground(Color.green);
@@ -46,14 +53,31 @@ public class BuyTickets {
                 searchbutton.setFont(new Font("serif", 0, 20));
                 buyTicketsPanel.add(searchbutton);
 
-                printbuttons();
+                //coonfirm search button
+                couponButton.setBackground(Color.gray);
+                couponButton.setBounds(200, 0, 200, 25);
+                couponButton.setFocusPainted(false);
+                couponButton.setFont(new Font("serif", 0, 20));
+                buyTicketsPanel.add(couponButton);
+                couponButton.addActionListener(new ActionListener() {
+                        public void actionPerformed(ActionEvent actionEvent) {
+                                if (coupon) {
+                                        coupon = false;
+                                        couponButton.setBackground(Color.gray);
+                                }
+                                else {
+                                        coupon = true;
+                                        couponButton.setBackground(Color.blue);
+                                }
+                        }
+                });
 
                 searchbutton.addActionListener(new ActionListener() {
                         public void actionPerformed(ActionEvent e) {
                                 Component[] components = buyTicketsPanel.getComponents();
 
                                 for (Component component : components) {
-                                        if (!(component == search || component == searchbutton))
+                                        if (!(component == search || component == searchbutton || component == ticketToBuy || component == couponButton))
                                         buyTicketsPanel.remove(component);
                                 }
                                 buyTicketsPanel.revalidate();
@@ -68,31 +92,38 @@ public class BuyTickets {
                 });
 
 
+                Login.adminFrame.add(buyTicketsPanel);
         }
                 private void printbuttons(){
                 try {
-                        Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "MagicalDay9296");
+                        Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "oddschool0");
                         Statement statement = connection.createStatement();
                         ResultSet resultset = statement.executeQuery(
-                                "select * from piljetter.bands b, piljetter.concerts c, piljetter.venues v " +
-                                        "WHERE b.band_id = c.band_id and v.venue_id = c.venue_id and " +
-                                        "(UPPER (b.band_name) like UPPER ('%"+searchword+"%')) and " + //searchword case insensetive
-                                        "(c.start_date) > current_timestamp"); //filterar ut de konserter som redan varit
+                                "select DISTINCT * from piljetter.bands b, piljetter.concerts c, piljetter.venues v WHERE b.band_id = c.band_id and v.venue_id = c.venue_id " +
+                                        "and ((UPPER (b.band_name) like UPPER ('%"+searchword+"%')) " +
+                                        "or (UPPER (v.venue_name) like UPPER ('%"+searchword+"%')) " +
+                                        "or (UPPER (v.country) like UPPER ('%"+searchword+"%')) " +
+                                        "or (UPPER (v.city) like UPPER ('%"+searchword+"%')) " +
+                                        "or ((c.date <= '"+searchword+"') and (c.date >= current_date)) " +
+
+
+                                        "and (c.date) > current_date"); //filterar ut de konserter som redan varit
 
 
                         while(resultset.next()){
-                                                System.out.println(resultset.getString(2)); //i = kolumnen
-                                                buttonTextBand = resultset.getString(2); //band
-                                                buttonTextVenue = resultset.getString(10); //arena / venue
-                                                buttonTextDate = resultset.getString(8); //date & time
-                                                buttonTextCountry = resultset.getString(11); //date & time
+                                //System.out.println(resultset.getString("band_name")); //s = kolumnen
+                                                buttonTextBand = resultset.getString("band_name"); //band
+                                                buttonTextVenue = resultset.getString("venue_name"); //arena / venue
+                                                buttonTextDate = resultset.getString("date"); //date & time
+                                                buttonTextCountry = resultset.getString("country"); //country
+                                                buttonConcertId = resultset.getString("concert_id"); //c id
 
 
-                                                new bandButton(buttonTextBand);
-                                                new bandButton(buttonTextVenue);
-                                                new bandButton(buttonTextDate);
-                                                new bandButton(buttonTextCountry);
-                                                i++;
+
+                                                new bandButton(buttonTextBand,buttonConcertId);
+                                                new bandButton(buttonTextVenue,buttonConcertId);
+                                                new bandButton(buttonTextDate,buttonConcertId);
+                                                new bandButton(buttonTextCountry,buttonConcertId);
                         }
                 } catch (Exception var3) {
                         System.out.println(var3.toString());
--- src/piljetter/bandButton.java
+++ src/piljetter/bandButton.java
@@ -1,29 +1,121 @@
-package piljetter;
+package com.company;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.*;
+import java.util.ArrayList;
 
 public class bandButton {
 
     public static int buttonPositionX = 50;
     public static int buttonPositionY = 50;
     static JButton bandButton;
+    int couponsUsed;
 
-    bandButton(String buttonTextBand){
-        button(buttonTextBand);
+
+    bandButton(String buttonTextBand, String buttonConcertId){
+        button(buttonTextBand, buttonConcertId);
     }
 
-                private void button(String buttonTextBand) {
+                private void button(String buttonTextBand, String buttonConcertId) {
                     bandButton = new JButton(buttonTextBand);
                     bandButton.setForeground(Color.black);
                     bandButton.setBackground(Color.gray);
                     bandButton.setBounds(buttonPositionX, buttonPositionY, 200, 25);
                     bandButton.setFocusPainted(false);
                     bandButton.setFont(new Font("serif", 0, 20));
-                    BuyTickets.buyTicketsPanel.add(bandButton);
+                    buyTickets.buyTicketsPanel.add(bandButton);
 
-                    //System.out.println(buttonPositionY);
+                    bandButton.addActionListener(new ActionListener() {
+                                                     public void actionPerformed(ActionEvent actionEvent) {
+                                                         try {
+                                                             //gets ticket_amount
+                                                                     Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres",                                                                           "oddschool0");
+                                                                     Statement statement = connection.createStatement();
+                                                                    ResultSet resultset = statement.executeQuery(
+                                                                     "select * from piljetter.concerts c, piljetter.user_accounts u WHERE (c.concert_id = ('" + buttonConcertId + "')) and (u.customer_id = '"+loggedIn.currentUser+"') ");
+                                                                     //change customer id to currently loged in user
+
+                                                             //removes expired tickets
+                                                             Statement statement6 = connection.createStatement();
+                                                             statement6.execute("delete from piljetter.user_coupons where expiry_date < current_date ");
+
+
+
+                                                             Statement statement3 = connection.createStatement();
+                                                             ResultSet resultset3 = statement3.executeQuery("select Count(*) from piljetter.user_coupons where customer_id = '"+loggedIn.currentUser+"'");
+                                                             resultset3.next();
+                                                             int couponAmount = Integer.parseInt(resultset3.getString("count"));
+                                                             //change customer id to currently loged in user
+
+
+                                                             while (resultset.next()) {
+                                                                         //if there are enough tickets left on the concert
+                                                                        //and you can afford them or you can afford them with coupons and "use coupon" is checked
+                                                                         if ((Integer.parseInt(resultset.getString("ticket_amount")) > Integer.parseInt(buyTickets.ticketToBuy.getText())) &&
+                                                                                 ((Integer.parseInt(resultset.getString("pesetas_balance")) > (Integer.parseInt(resultset.getString("price"))*Integer.parseInt(buyTickets.ticketToBuy.getText())))
+                                                                           || (buyTickets.coupon &&(Integer.parseInt(resultset.getString("pesetas_balance")) > (Integer.parseInt(resultset.getString("price"))*(Integer.parseInt(buyTickets.ticketToBuy.getText()) - couponAmount))))))
+                                                                         {
+
+                                                                             if (couponAmount<Integer.parseInt(buyTickets.ticketToBuy.getText())){
+                                                                                 couponsUsed = couponAmount;
+                                                                             }
+                                                                             else {
+                                                                                 couponsUsed = Integer.parseInt(buyTickets.ticketToBuy.getText());
+                                                                             }
+
+                                                                             if (buyTickets.coupon) {
+                                                                                 Statement statement4 = connection.createStatement();
+                                                                                 ResultSet resultset4 = statement4.executeQuery("select coupon_id from piljetter.user_coupons where customer_id = '"+loggedIn.currentUser+"' order by expiry_date desc limit '" + couponsUsed + "' ");
+                                                                                 //change customer id to currently loged in user
+
+                                                                                 while (resultset4.next()) {
+                                                                                     Statement statement5 = connection.createStatement();
+                                                                                     statement5.execute("delete from piljetter.user_coupons where coupon_id = '" + Integer.parseInt(resultset4.getString("coupon_id")) + "' ");
+                                                                                 }
+                                                                             }
+
+                                                                             Statement statement2 = connection.createStatement();
+                                                                             statement2.execute("update piljetter.concerts set ticket_amount = (ticket_amount - ('"+Integer.parseInt(buyTickets.ticketToBuy.getText())+"')) where concert_id = ('"+buttonConcertId+"')");
+                                                                             statement2.execute("update piljetter.user_accounts set pesetas_balance = pesetas_balance - ('"+Integer.parseInt(resultset.getString("price"))*(Integer.parseInt(buyTickets.ticketToBuy.getText())-couponsUsed)+"') where customer_id = '"+loggedIn.currentUser+"'");
+                                                                             //change customer id to currently loged in user
+
+
+                                                                             Statement statement7 = connection.createStatement();
+                                                                             statement7.execute("insert into piljetter.bought_tickets (concert_id, customer_id, ticket_amount, purchase_date, total_cost, coupons_used) " +
+                                                                                     "values('"+buttonConcertId+"', '"+loggedIn.currentUser+"', '"+Integer.parseInt(buyTickets.ticketToBuy.getText())+"', current_date,('"+Integer.parseInt(resultset.getString("price"))*Integer.parseInt(buyTickets.ticketToBuy.getText())+"'), '"+couponsUsed+"')");
+                                                                             //change customer id to currently loged in user
+
+
+
+
+
+
+                                                                             System.out.println("successful purchase");
+                                                                         }
+                                                                         else if (Integer.parseInt(resultset.getString("ticket_amount")) < Integer.parseInt(buyTickets.ticketToBuy.getText())) {
+                                                                             System.out.println("there are not enough tickets for your purchase");
+                                                                         }
+                                                                             else if(Integer.parseInt(resultset.getString("pesetas_balance")) < (Integer.parseInt(resultset.getString("price"))*Integer.parseInt(buyTickets.ticketToBuy.getText()))){
+                                                                             System.out.println("you can't afford that");
+                                                                         }
+                                                                             else {
+                                                                             System.out.println("you found an unexpected error");
+                                                                         }
+                                                                     }
+
+                                                                 } catch (Exception e) {
+                                                             System.out.println(e.toString());
+                                                             System.out.println("incorrect input, enter a number");
+                                                         }
+                                                     }
+                                                 });
+
+                        //System.out.println(buttonPositionY);
                     if (buttonPositionX < 650 )buttonPositionX += 200;
                     else {buttonPositionX = 100; buttonPositionY += 25;};
                 }
 }
+
